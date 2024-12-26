b7c729bfe3bbbc414bb6d32e3a675f42e1fe082d
PrathikshaPadmaprasad <prathiksha.padmaprasad@gmail.com>
2024-12-26 18:06:45 +1100

    Built a resposnive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { defaultSystem } from "@chakra-ui/react";
+
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
