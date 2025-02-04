a0b298ed0706206a0fe86575657d6fa67ae91716
PrathikshaPadmaprasad <prathiksha.padmaprasad@gmail.com>
2025-02-05 09:52:05 +1100

    Build search input

--- src/Components/ColorModeSwitch.tsx
+++ src/Components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>DarkMode</Text>
+      <Text whiteSpace="nowrap">DarkMode</Text>
     </HStack>
   );
 };
